5a358bcbef9fb0c10c125e70e063b0c724838e77
Development482 <Development482@users.noreply.github.com>
2019-09-24 12:27:20 +0300

    Uploading cache info

---
+++ 3b871d4/cppzmq/ce1f05bdc6858c47c6d52a87d0835ecab04f38e8/ce1f05b/4a1f8c7/args.cmake
@@ -0,0 +1,2 @@
+set("CMAKE_PREFIX_PATH" "/home/bogdan/.hunter/_Base/f7dece3/3b871d4/d194491/Install" CACHE INTERNAL "")
+set("ZeroMQ_DIR" "/home/bogdan/.hunter/_Base/f7dece3/3b871d4/d194491/Install/lib/cmake/ZeroMQ" CACHE INTERNAL "")
